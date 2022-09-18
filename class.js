511cdb1d6a1e83fc6d5e1d58b6cdb13d0db94245
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-09-18 19:11:01 +0000

    got class Clock ticking

--- class.js
+++ class.js
@@ -1,33 +1,30 @@
-const ClockText = document.getElementById('ClockText')
+const ClockText = document.getElementById('ClockText');
 
 class Clock {
-    constructor (days, year, month, date, day, hour, seconds) {
+    constructor (day, year, month, date, hour, second) {
         this.year = year;
-        this.days = days;
+        this.day = day;
         this.month = month;
         this.date = date;
         this.day = day;
         this.hour = hour;
-        this.seconds = seconds;
+        this.second = second;
+    }
+    updateClock() {
+        let dateObj = new Date()
+
+        let day = dateObj.getDay();
+        let year = dateObj.getFullYear();
+        let month = dateObj.getMonth();
+        let date = dateObj.getDate();
+        let hour = dateObj.getHours();
+        let second = dateObj.getSeconds();
+        
+        ClockText.textContent = `${day} ${month} ${date}, ${year},${hour}:${second}`;
     }
 }
 
-class Alarm extends Clock {
-    
-}
 
+const clock1 = new Clock().updateClock
 
-function updateClock(){
-    let dateObj = new Date();
-    let days = dateObj.getDay();
-    let years = dateObj.getFullYear();
-    let months = dateObj.getMonth();
-    let date = dateObj.getDate();
-    let hour = dateObj.getHours();
-    let seconds = dateObj.getSeconds();
-
-    let clock1 = new Clock(days ,years, months, date, hour, seconds)
-    ClockText.textContent = `${days} ${months} ${date}, ${years},${hour}:${seconds}`
-}
-
-setInterval(updateClock, 1000)
+setInterval(clock1, 1000);
