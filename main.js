69a73ad93e0178e8df0dfe69a9f2a8e004911602
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-10-03 17:20:36 +0000

    api

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const apiKey ='oZ3eyA5UiQi4R8DC13nQEk_szo2VumDHpCHFX8zMICd2OS-1x8eZgII_NrafJh2n4anEHRbKj1T1I40rKderB_fDKcnQLQZZh3BXqNOL3byZRfwFcwHsTAoRgxc7Y3Yx';
 // client ID : nxS67I04wcNhx-Ikf4dmYg
 
+async function getYelpInfo(){
+    let url = 'https://api.yelp.com/v3/businesses/search';
+    let response = await axios.get(url)
+    console.log(response)
+}
+getYelpInfo()
